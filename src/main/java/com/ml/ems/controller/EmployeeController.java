c7e3c59a1a9b2131e70067ff2e4fb4415e24feda
Himanshu Rai <swavyast@gmail.com>
2024-03-18 10:58:46 +0530

    commit - 000000000000000000000000003

--- src/main/java/com/ml/ems/controller/EmployeeController.java
+++ src/main/java/com/ml/ems/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.ml.ems.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ml.ems.model.Employee;
 import com.ml.ems.repository.EmployeeRepository;
 
+@CrossOrigin(origins = {"http://localhost:3000/"})
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
--- src/main/java/com/ml/ems/model/Employee.java
+++ src/main/java/com/ml/ems/model/Employee.java
@@ -26,4 +26,35 @@ public class Employee {
 	private String email;
 	@Column(name = "phone")
 	private String phone;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
 }
